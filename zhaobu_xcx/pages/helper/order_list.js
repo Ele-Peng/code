2fc89746f33dfd7c60f4cd1f0146d50755f03564
Ele-Peng <374237925@qq.com>
2018-08-01 20:59:11 +0800

    帮助文档完善2.0

--- zhaobu_xcx/pages/helper/order_list.js
+++ zhaobu_xcx/pages/helper/order_list.js
@@ -1,37 +1,32 @@
-// pages/helper/index.js
-
-import {
-  $wuxDialog
-} from '../../components/wux'
+// pages/helper/order_list.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    heading: "帮助与反馈",
-    lastUrl: "",
-    index: 0
+    heading: "我的订购记录",
+    lastUrl: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-
+  onLoad: function (options) {
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
-
+  onReady: function () {
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
     //获得title 标题栏组件
     this.title = this.selectComponent("#title");
     getApp().globalData.lastUrl = '../order/order_list'
@@ -43,54 +38,35 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
-
+  onHide: function () {
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
-
+  onUnload: function () {
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
-
+  onPullDownRefresh: function () {
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
-
+  onReachBottom: function () {
+  
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
-
-  },
-
-  onchange: function(e) {
-    console.log(e);
-    this.setData({
-      index: e.target.dataset.index
-    });
-  },
-
-  onclickPhone: function(e) {
-    wx.makePhoneCall({
-      phoneNumber: '0572-3158811',
-      success: function() {
-        console.log("拨打电话成功！")
-      },
-      fail: function() {
-        console.log("拨打电话失败！")
-      }
-    })
+  onShareAppMessage: function () {
+  
   }
 })
