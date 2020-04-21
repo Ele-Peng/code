f43de607b37cd28fa8d998b7323ca3a7921ce63e
pengqian05 <374237925@qq.com>
2020-04-21 14:36:00 +0800

    1: 15 三数之和

---
+++ A题/leetcode/15.三数之和.js
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=javascript
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+// 暴力求解 O(n^3)
+// 会超时 Time Limit Exceeded
+// k + j + i = 0
+// j + i = -k
+// var threeSum = function(nums) {
+//   let res = []
+//   nums.sort((a, b) => { return a - b})
+//   for (let k = 0; k < nums.length - 2; k ++) {
+//     for (let j = k + 1; j < nums.length - 1; j ++) {
+//       for (let i = j + 1; i < nums.length; i ++) {
+//         if (nums[k] + nums[j] + nums[i] === 0) {
+//           if (res.length === 0) {
+//             res.push([nums[k], nums[j], nums[i]])
+//           }
+//           let isExitInRes = false
+//           for (let resI = 0; resI < res.length; resI ++) {
+//             if ((res[resI][0] === nums[k]) && (res[resI][1] === nums[j]) && (res[resI][2] === nums[i])) {
+//               isExitInRes = true
+//             }
+//           }
+//           if (!isExitInRes) {
+//             res.push([nums[k], nums[j], nums[i]])
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return res
+// };
+
+
+// hash 来记录
+
+
+// 双指针 左右下标
+var threeSum = function(nums) {
+  let res = []
+  nums.sort((a, b) => { return a - b })
+  for (let target = 0;target < nums.length; target++) {
+    if (nums[target] > 0) break;
+    if (target > 0 && nums[target] === nums[target - 1]) {
+      continue;
+    };
+    for (let i = target + 1, j = nums.length - 1; i < j; ) {
+      if (nums[target] + nums[i] + nums[j] < 0) {
+        i++
+        while (i < j && nums[i] === nums[i - 1]) i++
+      } else if (nums[target] + nums[i] + nums[j] > 0) {
+        j--
+        while (i < j && nums[j] === nums[j + 1]) j--
+      } else if (nums[target] + nums[i] + nums[j] === 0) {
+        res.push([nums[target], nums[i], nums[j]])
+        i++
+        j--
+        while (i < j && nums[i] === nums[i - 1]) i++
+        while (i < j && nums[j] === nums[j + 1]) j--
+      }
+    }
+  }
+  return res
+};
+// @lc code=end
+
