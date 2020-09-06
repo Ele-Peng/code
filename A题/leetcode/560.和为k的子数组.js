fc21fde7c768ad1e5b1ead28900284a15a4b1521
ElePeng <374237925@qq.com>
2020-09-06 13:42:24 +0800

    update: 前缀和优化

--- A题/leetcode/560.和为k的子数组.js
+++ A题/leetcode/560.和为k的子数组.js
@@ -11,19 +11,18 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    let preSum = [0];
-    for (let i = 0; i < nums.length; i ++) {
-        preSum[i + 1] = nums[i] + preSum[i];
-    }
+    let preSum = {
+        "0": 1
+    };
+    let sum_i = 0;
     let ans = 0;
-    for (let i = 1; i <= nums.length; i ++) {
-        for (let j = 0; j < i; j ++) {
-            if (preSum[i] - preSum[j] === k) {
-                ans ++;
-            }
+    for (let i = 0; i < nums.length; i ++) {
+        sum_i = sum_i + nums[i];
+        if (preSum[`${sum_i - k}`] !== void 0) {
+            ans += preSum[`${sum_i - k}`];
         }
+        preSum[`${sum_i}`] = (preSum[`${sum_i}`] || 0) + 1;
     }
     return ans;
 };
 // @lc code=end
-
