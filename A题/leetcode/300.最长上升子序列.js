493b1edd864eaff346e8488212e59051acd651c0
Ele-Peng <374237925@qq.com>
2020-09-07 11:38:27 +0800

    feat: lcs-二分查找

--- A题/leetcode/300.最长上升子序列.js
+++ A题/leetcode/300.最长上升子序列.js
@@ -10,25 +10,27 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
-    if (nums.length === 0) return 0;
-    if (nums.length === 1) return 1;
-    let f = new Array(nums.length + 1).fill(1);
-    for (let i = 1; i < nums.length; i ++) {
-        // 第 j 元素，前 i 个元素
-        for (let j = 0; j < i; j ++) {
-            // console.log(f);
-            if (nums[i] > nums[j] && f[j] + 1 > f[i]) {
-                f[i] = f[j] + 1;
-            }
-        }
+  let top = new Array(nums.length);
+  let piles = 0; // 牌堆数初始化为0
+  for (let i = 0; i < nums.length; i ++) {
+    // 要处理的扑克牌
+    let poker = nums[i];
+
+    /** 二分查找放置位置 **/
+    let left = 0, right = piles;
+    while(left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (top[mid] < poker) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
     }
-    let ans = 0;
-    for (let i = 0; i < nums.length; i ++) {
-        if (f[i] > ans) {
-            ans = f[i]
-        }
-    }
-    return ans;
+
+    if (left === piles) piles++;
+    top[left] = poker;
+  }
+  return piles;
 };
 // @lc code=end
 
