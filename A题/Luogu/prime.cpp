3db60200c184c057dd4485829b882f1c1f217980
Ele-Peng <374237925@qq.com>
2020-09-15 10:03:27 +0800

    feat: prime

--- A题/Luogu/prime.cpp
+++ A题/Luogu/prime.cpp
@@ -9,14 +9,20 @@ int main() {
     cin >> num;
     int _num = num;
     int n = 0;
-    
     while(n > 0) {
         _num = n * 10 + _num;
         n = num % 10;
     }
-    if (num < 2) return false;
+    if (num < 2) {
+      cout << "no" << endl;
+      return 0;
+    };
     for (int i = 2; i < sqrt(num); i ++) {
-        if (num % i == 0) return false;
+      if (num % i == 0) {
+        cout << "no" << endl;
+        return 0;
+      }
     }
-    return true;
+    cout << "yes" << endl;
+    return 0;
 }
