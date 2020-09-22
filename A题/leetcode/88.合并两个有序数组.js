a8c89d7e49514230d7dc4144497be2d706022ccd
ElePeng <374237925@qq.com>
2020-09-23 01:02:21 +0800

    feat: 合并两个有序数组

---
+++ A题/leetcode/88.合并两个有序数组.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=javascript
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let nums1Tail = m - 1;
+    let nums2Tail = n - 1;
+    let len = m + n - 1;
+    while (nums1Tail >= 0 && nums2Tail >= 0) {
+        nums1[len --] = nums1[nums1Tail] > nums2[nums2Tail] ? nums1[nums1Tail--] : nums2[nums2Tail--];
+    }
+};
+// @lc code=end
+
