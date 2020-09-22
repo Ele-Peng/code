f35de1bd40792df44adc33a8ea5b72935d6c2d01
ElePeng <374237925@qq.com>
2020-09-22 23:25:46 +0800

    feat: 141. 环形链表

---
+++ A题/leetcode/141.环形链表.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let slow = head;
+    let quick = head;
+    while(quick) {
+        if (quick.next == null) return false;
+        slow = slow.next;
+        quick = quick.next.next;
+        if (quick == slow) return true;
+    }
+    return false;
+};
+// @lc code=end
+
