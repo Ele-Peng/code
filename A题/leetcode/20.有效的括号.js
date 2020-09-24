a2107f630a2f909b501113b9067e7756d21fcaa3
Elle-Peng <EllePeng@aliyun.com>
2020-09-24 21:21:33 +0800

    feat: 20. 有效的括号

---
+++ A题/leetcode/20.有效的括号.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  let str = s.split("");
+  let stack = [];
+  let symbolMap = {
+    "}": "{",
+    ")": "(",
+    "]": "["
+  }
+  for (let i = 0; i < str.length; i ++) {
+    if (str[i] === "{" || 
+      str[i] === "[" ||
+      str[i] === "(") {
+        stack.push(str[i]);
+    } else {
+      let temp = stack.pop();
+      if (symbolMap[str[i]] !== temp) {
+        return false
+      }
+    }
+  }
+  return !stack.length && true;
+};
+// @lc code=end
+
