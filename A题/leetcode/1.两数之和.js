38a0bad825d92cdce12c5be2ea46c6e3b7988eab
ElePeng <374237925@qq.com>
2020-09-26 16:59:37 +0800

    feat: 1 两数之和

--- A题/leetcode/1.两数之和.js
+++ A题/leetcode/1.两数之和.js
@@ -10,7 +10,29 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// Map O(nlogn)
+// var twoSum = function(nums, target) {
+//     let map = {};
+//     for (let i = 0; i < nums.length; i ++) {
+//         if (map[target - nums[i]] !== void 0 && (map[target - nums[i]] !== i)) return [map[target - nums[i]], i];
+//         map[nums[i]] = i;
+//     }
+// };
+
+
+// 双指针
+
 var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length - 1; i ++) {
+        for (let j = i + 1; j < nums.length; j ++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
 };
+
+
 // @lc code=end
 
