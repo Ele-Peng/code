b0670c3fc6223d25869259859994876e3c0b2a2a
Ele-Peng <374237925@qq.com>
2020-09-28 16:40:28 +0800

    feat: 旋转数组暴力

--- A题/leetcode/189.旋转数组.js
+++ A题/leetcode/189.旋转数组.js
@@ -11,7 +11,7 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-// space O(n)
+// space O(n) 但申请了一个新数组
 // var rotate = function(nums, k) {
 //   let res = new Array(nums.length);
 //   for (let i = 0; i < nums.length; i ++) {
@@ -22,15 +22,19 @@
 //   }
 // };
 
-//
+// 暴力 O(n * k)
 var rotate = function(nums, k) {
-    for (let i = 0; i < nums.length; i ++) {
-        let temp = nums[i];
-        nums[i] = nums[(i + k) % nums.length];
-        nums[(i + k) % nums.length] = temp;
-        console.log(nums);
-        console.log("=====");
+  let tempEnd, tempStart;
+  for (let i = 0; i < k; i ++) {
+    tempEnd = nums[nums.length - 1];
+    for (let j = 0; j < nums.length; j ++) {
+      tempStart = nums[j];
+      nums[j] = tempEnd;
+      tempEnd = tempStart;
     }
+  }
 };
+
+
 // @lc code=end
 
