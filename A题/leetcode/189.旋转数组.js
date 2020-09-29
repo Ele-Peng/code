a484f1f4f8015fa315f9d33832c9baeb9a659c4f
Ele-Peng <374237925@qq.com>
2020-09-29 10:28:39 +0800

    189. 旋转数组旋转三次

--- A题/leetcode/189.旋转数组.js
+++ A题/leetcode/189.旋转数组.js
@@ -23,18 +23,39 @@
 // };
 
 // 暴力 O(n * k)
+// var rotate = function(nums, k) {
+//   let tempEnd, tempStart;
+//   for (let i = 0; i < k; i ++) {
+//     tempEnd = nums[nums.length - 1];
+//     for (let j = 0; j < nums.length; j ++) {
+//       tempStart = nums[j];
+//       nums[j] = tempEnd;
+//       tempEnd = tempStart;
+//     }
+//   }
+// };
+
+// 三次交换
 var rotate = function(nums, k) {
-  let tempEnd, tempStart;
-  for (let i = 0; i < k; i ++) {
-    tempEnd = nums[nums.length - 1];
-    for (let j = 0; j < nums.length; j ++) {
-      tempStart = nums[j];
-      nums[j] = tempEnd;
-      tempEnd = tempStart;
+  let n = nums.length;
+  k %= n;
+  if (n == 1) {
+    return;
+  }
+  let temp = 0;
+  rotateHelper(0, n - 1);
+  rotateHelper(0, k - 1);
+  rotateHelper(k, n - 1);
+  function rotateHelper(start, end) {
+    while(start < end) {
+      temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start ++;
+      end --;
     }
   }
 };
-
 
 // @lc code=end
 
