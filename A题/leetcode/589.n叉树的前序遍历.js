cb87a98b9aee5286913d33c8b17536a26665695d
Ele-Peng <374237925@qq.com>
2020-10-12 17:55:09 +0800

    feat: n叉树的前序遍历-迭代

--- A题/leetcode/589.n叉树的前序遍历.js
+++ A题/leetcode/589.n叉树的前序遍历.js
@@ -17,18 +17,32 @@
  * @param {Node} root
  * @return {number[]}
  */
+// 递归
+// var preorder = function(root) {
+//   let res = [];
+//   preorderHelper(root, res);
+//   return res;
+// };
+
+// var preorderHelper = function(root, arr) {
+//   if (!root) return root;
+//   arr.push(root.val); // 修改 arr 引用
+//   for (let i = 0; i < root.children.length; i ++) {
+//     preorderHelper(root.children[i], arr);
+//   }
+// }
+
+// 迭代 利用队列
 var preorder = function(root) {
-  let res = [];
-  preorderHelper(root, res);
+  if (!root) return [];
+  let res = [root.val];
+  let queue = [...root.children];
+  while(queue.length) {
+    let cur = queue.shift();
+    res.push(cur.val);
+    queue.unshift(...cur.children);
+  }
   return res;
 };
-
-var preorderHelper = function(root, arr) {
-  if (!root) return root;
-  arr.push(root.val); // 修改 arr 引用
-  for (let i = 0; i < root.children.length; i ++) {
-    preorderHelper(root.children[i], arr);
-  }
-}
 // @lc code=end
 
