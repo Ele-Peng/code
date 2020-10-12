fbd155bb9605d501b9ec9d980676197eb98ebde9
Ele-Peng <374237925@qq.com>
2020-10-12 17:02:42 +0800

    update: 49.字母异位词分组优化

--- A题/leetcode/49.字母异位词分组.js
+++ A题/leetcode/49.字母异位词分组.js
@@ -9,22 +9,39 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+// 预处理下标
+// var groupAnagrams = function(strs) {
+//   // 预处理数组，形成一个对应的 charcode 数组
+//   let strsTemp = {};
+//   for (let i = 0;i < strs.length; i ++) {
+//     let temp = strs[i].split("").sort().join("");
+//     strsTemp[temp] ? strsTemp[temp].push(i) : strsTemp[temp] = [i]; // 存的是下标
+//   }
+//   let res = [];
+//   let keys = Object.keys(strsTemp);
+//   for (let i = 0; i < keys.length; i ++) {
+//     let temp = [];
+//     let key = strsTemp[keys[i]]
+//     for (let j = 0; j < key.length; j ++) {
+//       temp.push(strs[key[j]]);
+//     }
+//     res.push(temp);
+//   }
+//   return res;
+// };
+
+
+// 优化预处理直接为该数组 + for in Object
+
 var groupAnagrams = function(strs) {
-  // 预处理数组，形成一个对应的 charcode 数组
   let strsTemp = {};
   for (let i = 0;i < strs.length; i ++) {
-    let temp = strs[i].split("").sort().join("");
-    strsTemp[temp] ? strsTemp[temp].push(i) : strsTemp[temp] = [i]; // 存的是下标
+    let key = strs[i].split("").sort().join("");
+    strsTemp[key] ? strsTemp[key].push(strs[i]) : strsTemp[key] = [strs[i]];
   }
   let res = [];
-  let keys = Object.keys(strsTemp);
-  for (let i = 0; i < keys.length; i ++) {
-    let temp = [];
-    let key = strsTemp[keys[i]]
-    for (let j = 0; j < key.length; j ++) {
-      temp.push(strs[key[j]]);
-    }
-    res.push(temp);
+  for (let i in strsTemp) {
+    res.push(strsTemp[i]);
   }
   return res;
 };
