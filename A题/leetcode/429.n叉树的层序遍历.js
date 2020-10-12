31c7f1b2263329a708eaa5a9d3800ebd6fdcd7b3
ElePeng <374237925@qq.com>
2020-10-13 00:05:04 +0800

    update: 429.n叉树的层序遍历dfs

--- A题/leetcode/429.n叉树的层序遍历.js
+++ A题/leetcode/429.n叉树的层序遍历.js
@@ -18,21 +18,42 @@
  * @return {number[][]}
  */
 // 层序遍历递归
+// var levelOrder = function(root) {
+//   if (!root) return [];
+//   const res = [];
+//   levelOrderHelper([root], res, 0);
+//   return res;
+// };
+// var levelOrderHelper = function(nodes, res, level) {
+//   if (!nodes.length) return nodes;
+//   if (!res[level]) res[level] = [];
+//   const childNodes = [];
+//   nodes.forEach(element => {
+//     res[level].push(element.val);
+//     childNodes.push(...element.children);
+//   });
+//   levelOrderHelper(childNodes, res, level += 1);
+// }
+
+// 层序遍历迭代
+
 var levelOrder = function(root) {
   if (!root) return [];
-  const res = [];
-  levelOrderHelper([root], res, 0);
+  let res = [];
+  root.level = 0;
+  let stack = [root];
+  while(stack.length) {
+    const node = stack.pop();
+    if (!res[node.level]) res[node.level] = [];
+    res[node.level].push(node.val);
+    if (node.children) {
+      for (let i =  node.children.length - 1; i >= 0; i --) {
+        node.children[i].level = node.level + 1;
+        stack.push(node.children[i]);
+      }
+    }
+  }
   return res;
 };
-var levelOrderHelper = function(nodes, res, level) {
-  if (!nodes.length) return nodes;
-  if (!res[level]) res[level] = [];
-  const childNodes = [];
-  nodes.forEach(element => {
-    res[level].push(element.val);
-    childNodes.push(...element.children);
-  });
-  levelOrderHelper(childNodes, res, level += 1);
-}
 // @lc code=end
 
