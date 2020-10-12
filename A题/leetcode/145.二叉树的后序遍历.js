9105208c92961cdf07eea213733979942f052fe4
ElePeng <374237925@qq.com>
2020-10-12 23:39:06 +0800

    feat: binary-tree-postorder-travelsal

--- A题/leetcode/145.二叉树的后序遍历.js
+++ A题/leetcode/145.二叉树的后序遍历.js
@@ -17,17 +17,44 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+// 递归
+// var postorderTraversal = function(root) {
+//     const res = [];
+//     postorderTraversalHelper(root, res);
+//     return res;
+// };
+
+// var postorderTraversalHelper = function(root, res) {
+//     if (!root) return root;
+//     postorderTraversalHelper(root.left, res);
+//     postorderTraversalHelper(root.right, res);
+//     res.push(root.val);
+// }
+
+
+// 迭代
+
 var postorderTraversal = function(root) {
-    const res = [];
-    postorderTraversalHelper(root, res);
+    if (!root) return [];
+    let res = [];
+    let stack = [];
+    let prev = null;
+    while(root || stack.length) {
+        while(root) {
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        if (root.right === null || root.right == prev) { // 叶子结点 或者 右节点已经读取过了，即读取根结点（重新入栈）
+            res.push(root.val);
+            prev = root;
+            root = null;
+        } else {
+            stack.push(root);
+            root = root.right;
+        }
+    }
     return res;
 };
-
-var postorderTraversalHelper = function(root, res) {
-    if (!root) return root;
-    postorderTraversalHelper(root.left, res);
-    postorderTraversalHelper(root.right, res);
-    res.push(root.val);
-}
 // @lc code=end
 
