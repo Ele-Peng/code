e838a97a388101f44ea302bcd135d91d4d483fcc
Ele-Peng <374237925@qq.com>
2020-10-12 16:01:54 +0800

    update: 242.有效的字母异位词更快的方法break

--- A题/leetcode/242.有效的字母异位词.js
+++ A题/leetcode/242.有效的字母异位词.js
@@ -10,17 +10,38 @@
  * @param {string} t
  * @return {boolean}
  */
+// 字母码值
+// var isAnagram = function(s, t) {
+//   let sArr = s.split("")
+//   let tArr = t.split("")
+//   if (sArr.length !== tArr.length) return false;
+//   let alphabet = new Array(26).fill(0);
+//   for (let i = 0;i < sArr.length; i ++) {
+//     alphabet[sArr[i].charCodeAt(0) - 'a'.charCodeAt(0)] ++;
+//     alphabet[tArr[i].charCodeAt(0) - 'a'.charCodeAt(0)] --;
+//   }
+//   for (let i = 0;i < alphabet.length; i ++) {
+//     if (typeof alphabet[i] !== "number" || alphabet[i] !== 0) return false;
+//   }
+//   return true;
+// };
+
+// 单纯的字母
+// more earlier to break 
 var isAnagram = function(s, t) {
   let sArr = s.split("")
   let tArr = t.split("")
   if (sArr.length !== tArr.length) return false;
-  let alphabet = new Array(26).fill(0);
+  let map = {};
   for (let i = 0;i < sArr.length; i ++) {
-    alphabet[sArr[i].charCodeAt(0) - 'a'.charCodeAt(0)] ++;
-    alphabet[tArr[i].charCodeAt(0) - 'a'.charCodeAt(0)] --;
+    map[sArr[i]] ? map[sArr[i]] ++ : map[sArr[i]] = 1;
   }
-  for (let i = 0;i < alphabet.length; i ++) {
-    if (typeof alphabet[i] !== "number" || alphabet[i] !== 0) return false;
+  for (let i = 0;i < tArr.length; i ++) {
+    if (map[tArr[i]]) {
+      map[tArr[i]] --;
+    } else {
+      return false;
+    }
   }
   return true;
 };
