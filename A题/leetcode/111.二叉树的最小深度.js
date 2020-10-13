15366c7983bb562ee96d28519ace4ddc07020829
ElePeng <374237925@qq.com>
2020-10-14 00:03:11 +0800

    feat: minimum-depth-od-binary-tree

--- A题/leetcode/111.二叉树的最小深度.js
+++ A题/leetcode/111.二叉树的最小深度.js
@@ -16,22 +16,33 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// var minDepth = function(root) {
+//   if (root == null) return 0;
+//   let ans = 1;
+//   let queue = [];
+//   queue.push(root);
+//   while(queue.length) {
+//     let size = queue.length;
+//     for (let i = 0; i < size; i ++) {
+//         let cur = queue.shift();
+//         if (!cur.left && !cur.right) return ans;
+//         if (cur.left != null) queue.push(cur.left);
+//         if (cur.right != null) queue.push(cur.right);
+//     }
+//     ans++;
+//   }
+//   return ans;
+// };
+
+
 var minDepth = function(root) {
+  // recursion teminator
   if (root == null) return 0;
-  let ans = 1;
-  let queue = [];
-  queue.push(root);
-  while(queue.length) {
-    let size = queue.length;
-    for (let i = 0; i < size; i ++) {
-        let cur = queue.shift();
-        if (!cur.left && !cur.right) return ans;
-        if (cur.left != null) queue.push(cur.left);
-        if (cur.right != null) queue.push(cur.right);
-    }
-    ans++;
-  }
-  return ans;
+
+  // logic process
+  else if (root.left === null) return minDepth(root.right) + 1;
+  else if (root.right === null) return minDepth(root.left) + 1;
+  else return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 };
 // @lc code=end
 
