af8b91a45d5f3854bde96430eae08cae383c9cba
ElePeng <374237925@qq.com>
2020-10-13 23:18:20 +0800

    feat: validate-binary-search-tree

--- A题/leetcode/98.验证二叉搜索树.js
+++ A题/leetcode/98.验证二叉搜索树.js
@@ -16,17 +16,33 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
-    return isValidBSTHelper(root, null, null);
+// var isValidBST = function(root) {
+//     return isValidBSTHelper(root, null, null);
     
+// };
+
+// function isValidBSTHelper(root, min, max) {
+//     if (root === null) return true;
+//     if (min != null && root.val <= min.val) return false;
+//     if (max != null && root.val >= max.val) return false;
+//     return isValidBSTHelper(root.left, min, root) &&
+//         isValidBSTHelper(root.right, root, max);
+// }
+
+
+var isValidBST = function(root) {
+    return isValidBSTHelper(root, null, null)
 };
 
-function isValidBSTHelper(root, min, max) {
-    if (root === null) return true;
-    if (min != null && root.val <= min.val) return false;
-    if (max != null && root.val >= max.val) return false;
+
+var isValidBSTHelper = function(root, min, max) {
+    // recursion terminator
+    if (!root) return true;
+    if (min !== null && root.val <= min.val) return false;
+    if (max !== null && root.val >= max.val) return false;
+
     return isValidBSTHelper(root.left, min, root) &&
-        isValidBSTHelper(root.right, root, max);
-}
+    isValidBSTHelper(root.right, root, max);
+};
 // @lc code=end
 
