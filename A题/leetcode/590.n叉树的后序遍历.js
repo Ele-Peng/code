86e6e62d40b3b0bef1898eee7d8be7f590298c1e
ElePeng <374237925@qq.com>
2020-10-18 17:34:42 +0800

    feat: 590. n叉树的后序遍历

--- A题/leetcode/590.n叉树的后序遍历.js
+++ A题/leetcode/590.n叉树的后序遍历.js
@@ -18,23 +18,39 @@
  * @return {number[]}
  */
 // 迭代 dfs
+// var postorder = function(root) {
+//     if (!root) return [];
+//     let stack = [root];
+//     let res = [];
+//     while (stack.length) {
+//         let cur = stack.pop();
+//         if (cur && cur.children.length) {
+//             stack.push(cur);
+//             for (let i = cur.children.length; i >= 0; i --) {
+//                 stack.push(cur.children[i]);
+//             }
+//             cur.children = []; // 子节点全部遍历过了
+//         } else if (cur && (cur.val !== null)) {
+//             res.push(cur.val);
+//         }
+//     }
+//     return res;
+// };
+
+// 递归
 var postorder = function(root) {
     if (!root) return [];
-    let stack = [root];
     let res = [];
-    while (stack.length) {
-        let cur = stack.pop();
-        if (cur && cur.children.length) {
-            stack.push(cur);
-            for (let i = cur.children.length; i >= 0; i --) {
-                stack.push(cur.children[i]);
-            }
-            cur.children = []; // 子节点全部遍历过了
-        } else if (cur && (cur.val !== null)) {
-            res.push(cur.val);
-        }
-    }
+    postorderHelper(root, res);
     return res;
+};
+var postorderHelper = function(root, res) {
+    if (root) {
+        for (let i = 0; i < root.children.length; i ++) {
+            postorderHelper(root.children[i], res);
+        }
+        res.push(root.val);
+    }
 };
 // @lc code=end
 
