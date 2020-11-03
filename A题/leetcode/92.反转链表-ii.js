d13f2fae8ff7f0d66c2a8fa34f7f4320bc2dae45
Ele-Peng <374237925@qq.com>
2020-11-03 11:46:11 +0800

    feat: 92.反转链表-ii

--- A题/leetcode/92.反转链表-ii.js
+++ A题/leetcode/92.反转链表-ii.js
@@ -18,23 +18,43 @@
  * @param {number} n
  * @return {ListNode}
  */
+// var reverseBetween = function(head, m, n) {
+//   if (m == 1) return reverseN(head, n);
+//   head.next = reverseBetween(head.next, m - 1, n - 1);
+//   return head;
+// };
+
+// // 反转链表前 N 个元素
+// let p = null; // 记录第 N 节点
+// function reverseN(head, n) {
+//   if (n == 1) {
+//     p = head.next;
+//     return head;
+//   }
+//   let last = reverseN(head.next, n - 1);
+//   head.next.next = head;
+//   head.next = p;
+//   return last;
+// }
 var reverseBetween = function(head, m, n) {
-  if (m == 1) return reverseN(head, n);
+  let tail = null;
+  var reverseN = function(head, n) {
+    // recursion terminator
+    if (!head || !head.next) return head;
+    // current level logic
+    if (n === 1) { // n 小于 链表长度
+      tail = head.next;
+      return head;
+    }
+    let next = head.next;
+    let reverseHead = reverseN(next, n - 1);
+    next.next = head;
+    head.next = tail;
+    return reverseHead;
+  }
+  if (m === 1) return reverseN(head, n);
   head.next = reverseBetween(head.next, m - 1, n - 1);
   return head;
 };
-
-// 反转链表前 N 个元素
-let p = null; // 记录第 N 节点
-function reverseN(head, n) {
-  if (n == 1) {
-    p = head.next;
-    return head;
-  }
-  let last = reverseN(head.next, n - 1);
-  head.next.next = head;
-  head.next = p;
-  return last;
-}
 // @lc code=end
 
