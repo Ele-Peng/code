37525420f5ff2d3767dc4280d4874a1911e43dc3
Ele-Peng <374237925@qq.com>
2020-11-09 14:30:51 +0800

    feat: maxSubArray

--- A题/leetcode/53.最大子序和.js
+++ A题/leetcode/53.最大子序和.js
@@ -9,17 +9,31 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var maxSubArray = function(nums) {
+//   let n = nums.length;
+//   if (n === 0) return 0;
+//   let dp = new Array(n);
+//   dp[0] = nums[0];
+//   for (let i = 1; i < n; i ++) {
+//     dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+//   }
+//   let res = dp[0];
+//   for (let i = 1; i < n; i ++) {
+//     res = Math.max(res, dp[i]);
+//   }
+//   return res;
+// };
+// dp 优化，空间复杂度 O(1)
 var maxSubArray = function(nums) {
   let n = nums.length;
   if (n === 0) return 0;
-  let dp = new Array(n);
-  dp[0] = nums[0];
+  let dp_default = nums[0];
+  let dp_res = nums[0];
+  let res = nums[0];
   for (let i = 1; i < n; i ++) {
-    dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
-  }
-  let res = dp[0];
-  for (let i = 1; i < n; i ++) {
-    res = Math.max(res, dp[i]);
+    dp_res = Math.max(nums[i], nums[i] + dp_default);
+    dp_default = dp_res;
+    res = Math.max(res, dp_res);
   }
   return res;
 };
