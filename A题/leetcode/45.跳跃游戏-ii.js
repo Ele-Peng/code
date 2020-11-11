6a0543f7cf3ca38f15b1b49c75c257e7a40da68d
Ele-Peng <374237925@qq.com>
2020-11-11 11:33:36 +0800

    feat: jump-ii

--- A题/leetcode/45.跳跃游戏-ii.js
+++ A题/leetcode/45.跳跃游戏-ii.js
@@ -9,21 +9,39 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var jump = function(nums) {
+//   let farthestPos = 0;
+//   let endOfCanReach = 0;
+//   let steps = 0;
+//   for (let i = 0; i < nums.length - 1; i ++) {
+//     farthestPos = Math.max(farthestPos, i + nums[i]);
+//     if (i === endOfCanReach) {
+//       endOfCanReach = farthestPos;
+//       steps++;
+//     }
+//     if (endOfCanReach >= nums.length - 1) {
+//       break;
+//     }
+//   }
+//   return steps;
+// };
+
+
 var jump = function(nums) {
-  let farthestPos = 0;
-  let endOfCanReach = 0;
-  let steps = 0;
-  for (let i = 0; i < nums.length - 1; i ++) {
-    farthestPos = Math.max(farthestPos, i + nums[i]);
-    if (i === endOfCanReach) {
-      endOfCanReach = farthestPos;
-      steps++;
-    }
-    if (endOfCanReach >= nums.length - 1) {
-      break;
+  let len = nums.length;
+  let end = 0; // 跳跃的暂时终点
+  let farthest = 0; // [i, ... end] 跳到的最远距离
+  let jump = 0; // 跳跃次数
+
+  for (let i = 0; i < len - 1; i ++) {
+    farthest = Math.max(farthest, nums[i] + i);
+    if (end === i) {
+      jump ++;
+      end = farthest;
     }
   }
-  return steps;
+
+  return jump;
 };
 // @lc code=end
 
