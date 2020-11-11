6534108854d89b8ae3a7740dc0ef978a07d184c7
Ele-Peng <374237925@qq.com>
2020-11-11 11:17:25 +0800

    feat: greedy canJump

--- A题/leetcode/55.跳跃游戏.js
+++ A题/leetcode/55.跳跃游戏.js
@@ -9,28 +9,39 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums) {
-    const memoryStack = new Array(nums.length);
-    memoryStack[nums.length - 1] = true;
-    const dfs = (level, nums) => {
-        // recursion terminator
-        if (memoryStack[level] !== void 0) { // 跳跃到数组的最后一个位置
-            return memoryStack[level];
-        }
+// var canJump = function(nums) {
+//     const memoryStack = new Array(nums.length);
+//     memoryStack[nums.length - 1] = true;
+//     const dfs = (level, nums) => {
+//         // recursion terminator
+//         if (memoryStack[level] !== void 0) { // 跳跃到数组的最后一个位置
+//             return memoryStack[level];
+//         }
 
-        // current level logic
-        let canJumpSteps = Math.min(level + nums[level], nums.length - 1); // 能跳跃的最远距离不能超过数组长度，且大于等于本位置至本位置索引的最大长度
-        // drill down
-        for (let i = level + 1; i <= canJumpSteps; i ++) {
-            if (dfs(i, nums)) {
-                memoryStack[level] = true;
-                return true;
-            }
-        }
-        memoryStack[level] = false;
-        return false;
-    }
-    return dfs(0, nums);
+//         // current level logic
+//         let canJumpSteps = Math.min(level + nums[level], nums.length - 1); // 能跳跃的最远距离不能超过数组长度，且大于等于本位置至本位置索引的最大长度
+//         // drill down
+//         for (let i = level + 1; i <= canJumpSteps; i ++) {
+//             if (dfs(i, nums)) {
+//                 memoryStack[level] = true;
+//                 return true;
+//             }
+//         }
+//         memoryStack[level] = false;
+//         return false;
+//     }
+//     return dfs(0, nums);
+// };
+
+// 贪心
+var canJump = function(nums) {
+  let len = nums.length;
+  let farthest = 0;
+  for (let i = 0; i < len - 1; i ++) {
+    farthest = Math.max(farthest, i + nums[i]);
+    if (farthest <= i) return false;
+  }
+  return farthest >= len - 1;
 };
 // @lc code=end
 
