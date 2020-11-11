c9a4ac66a732adf5f6fe84b003292ba8d7d11896
Ele-Peng <374237925@qq.com>
2020-11-11 10:20:00 +0800

    feat: canPartition

--- A题/leetcode/416.分割等和子集.js
+++ A题/leetcode/416.分割等和子集.js
@@ -11,33 +11,55 @@
  */
 
 // [1,2,3,5]
-var canPartition = function(nums) {
-  let sum = nums.reduceRight((previous, current) => {
-    return previous + current;
-  });
-  if (sum % 2) return false;
-  // 初始化 dp table
-  let len = sum >> 1;
-  let dp = new Array(nums.length + 1);
-  for (let i = 0; i <= nums.length; i ++) {
-    dp[i] = new Array(len + 1);
-    dp[0].fill(false);
-    dp[i][0] = true;
-  }
+// 二维 dp
+// var canPartition = function(nums) {
+//   let sum = nums.reduceRight((previous, current) => {
+//     return previous + current;
+//   });
+//   if (sum % 2) return false;
+//   // 初始化 dp table
+//   let len = sum >> 1;
+//   let dp = new Array(nums.length + 1);
+//   for (let i = 0; i <= nums.length; i ++) {
+//     dp[i] = new Array(len + 1);
+//     dp[0].fill(false);
+//     dp[i][0] = true;
+//   }
 
-  for (let i = 1; i <= nums.length; i ++) {
-    for (let j = 1; j <= len; j ++) {
-      if (j - nums[i - 1] < 0) { // 背包已经装不下，维持不装的状态
-        dp[i][j] = dp[i - 1][j];
-      } else { // 选择装或者 不装
-        dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
+//   for (let i = 1; i <= nums.length; i ++) {
+//     for (let j = 1; j <= len; j ++) {
+//       if (j - nums[i - 1] < 0) { // 背包已经装不下，维持不装的状态
+//         dp[i][j] = dp[i - 1][j];
+//       } else { // 选择装或者 不装
+//         dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
+//       }
+//     }
+//   }
+
+//   // console.log(dp);
+
+//   return dp[nums.length][len];
+// };
+
+// dp 优化
+var canPartition = function(nums) {
+  let sum = nums.reduceRight((accumulator, current) => {
+    return accumulator + current;
+  })
+  if (sum % 2) return false;
+  let mid = sum >> 1;
+
+  let dp = (new Array(mid + 1)).fill(false);
+  dp[0] = true;
+
+  for (let i = 0; i < nums.length; i ++) {
+    for (let j = mid; j >= 0; j --) {
+      if (j - nums[i] >= 0) {
+        dp[j] = dp[j] || dp[j - nums[i]];
       }
     }
   }
-
-  // console.log(dp);
-
-  return dp[nums.length][len];
-};
+  return dp[mid];
+}
 // @lc code=end
 
