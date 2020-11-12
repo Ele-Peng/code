2bc7b1464239f966c13054c0687be0d226b0742f
Ele-Peng <374237925@qq.com>
2020-11-12 16:58:23 +0800

    feat: dp 32. longestValidParentheses

--- A题/leetcode/32.最长有效括号.js
+++ A题/leetcode/32.最长有效括号.js
@@ -9,24 +9,52 @@
  * @param {string} s
  * @return {number}
  */
+// var longestValidParentheses = function(s) {
+//   let maxLen = 0;
+//   let stack = [];
+//   stack.push(-1);
+
+//   for (let i = 0; i < s.length; i ++) {
+//     if (s[i] === "(") {
+//       stack.push(i);
+//     } else {
+//       stack.pop();
+//       if (stack.length) {
+//         maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+//       } else {
+//         stack.push(i);
+//       }
+//     }
+//   }
+
+//   return maxLen;
+// };
+
+// dp
+
 var longestValidParentheses = function(s) {
   let maxLen = 0;
-  let stack = [];
-  stack.push(-1);
+  let len = s.length;
+  let dp = (new Array(len)).fill(0);
 
-  for (let i = 0; i < s.length; i ++) {
-    if (s[i] === "(") {
-      stack.push(i);
-    } else {
-      stack.pop();
-      if (stack.length) {
-        maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
-      } else {
-        stack.push(i);
+  for (let i = 0; i < len; i ++) {
+    if (s[i] === ")") {
+      if (s[i - 1] === "(") {
+        if (i - 2 >= 0) {
+          dp[i] = dp[i - 2] + 2;
+        } else {
+          dp[i] = 2;
+        }
+      } else if (s[i - dp[i - 1] - 1] === "("){
+        if (i - dp[i - 1] - 2 >= 0) {
+          dp[i] = dp[i - dp[i - 1] - 2] + dp[i - 1] + 2;
+        } else {
+          dp[i] = dp[i - 1] + 2;
+        }
       }
     }
+    maxLen = Math.max(maxLen, dp[i]);
   }
-
   return maxLen;
 };
 // @lc code=end
