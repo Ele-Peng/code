4f4bb9559689bb466fb9cd95d24668b3c7783076
Ele-Peng <374237925@qq.com>
2020-11-12 17:47:53 +0800

    feat: dp 72 edit distance

--- A题/leetcode/72.编辑距离.js
+++ A题/leetcode/72.编辑距离.js
@@ -30,17 +30,48 @@
 // };
 
 
+// var minDistance = function(word1, word2) {
+//   let len1 = word1.length;
+//   let len2 = word2.length;
+//   let dp = (new Array(len1 + 1));
+//   for (let i = 0; i <= len1; i ++) {
+//     dp[i] = (new Array(len2 + 1));
+//     dp[i][0] = i;
+//   }
+//   for (let j = 1; j <= len2; j ++) {
+//     dp[0][j] = j;
+//   }
+//   for (let i = 1; i <= len1; i ++) {
+//     for (let j = 1; j <= len2; j ++) {
+//       if (word1[i - 1] === word2[j - 1]) {
+//         dp[i][j] = dp[i - 1][j - 1];
+//       } else {
+//         dp[i][j] = Math.min(
+//           dp[i][j - 1],
+//           dp[i - 1][j],
+//           dp[i - 1][j - 1]
+//         ) + 1;
+//       }
+//     }
+//   }
+//   return dp[len1][len2];
+// };
+
+// dp 第二遍
 var minDistance = function(word1, word2) {
-  let len1 = word1.length;
-  let len2 = word2.length;
+  let len1 = word1.length; // 被转换 字符串
+  let len2 = word2.length; // 目标 字符串
+
+  // 初始化 dp table
   let dp = (new Array(len1 + 1));
   for (let i = 0; i <= len1; i ++) {
-    dp[i] = (new Array(len2 + 1));
+    dp[i] = new Array(len2 + 1);
     dp[i][0] = i;
   }
-  for (let j = 1; j <= len2; j ++) {
+  for (let j = 0; j <= len2; j ++) {
     dp[0][j] = j;
   }
+
   for (let i = 1; i <= len1; i ++) {
     for (let j = 1; j <= len2; j ++) {
       if (word1[i - 1] === word2[j - 1]) {
@@ -54,7 +85,8 @@ var minDistance = function(word1, word2) {
       }
     }
   }
+
   return dp[len1][len2];
-};
+}
 // @lc code=end
 
