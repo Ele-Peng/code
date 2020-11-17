b209914777a92755f071c927666a979169712480
ElePeng <374237925@qq.com>
2020-11-17 23:32:58 +0800

    feat: bubbleSort version 2

--- A题/leetcode/sort/bubbleSort.js
+++ A题/leetcode/sort/bubbleSort.js
@@ -1,13 +1,31 @@
-function bubbleSort(nums) {
-    if (!nums || !nums.length) return [];
-    for (let i = 0; i < nums.length - 1; i ++) { // 大循环比较 nums.length 次数
-        for (let j = 0; j < nums.length - i - 1; j ++) { // 小循环冒泡数组最大的值
+// function bubbleSort(nums) {
+//     if (!nums || !nums.length) return [];
+//     for (let i = 0; i < nums.length; i ++) { // 大循环比较 nums.length 次数
+//         for (let j = 0; j < nums.length - i - 1; j ++) { // 小循环冒泡数组最大的值
+//             if (nums[j] > nums[j + 1]) {
+//                 let temp = nums[j];
+//                 nums[j] = nums[ j + 1];
+//                 nums[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return nums;
+// }
+
+// 优化：
+// 如果经历 < nums.length 次，就已经有序了（不再进行冒泡）可以提前退出比较
+function bubbleSort() {
+    for (let i = 0; i < nums.length; i ++) {
+        let complete = true;
+        for (let j = 0; j < nums.length - i - 1; j ++) {
             if (nums[j] > nums[j + 1]) {
                 let temp = nums[j];
-                nums[j] = nums[ j + 1];
+                nums[j] = nums[j + 1];
                 nums[j + 1] = temp;
+                complete = false;
             }
         }
+        if (complete) break;
     }
     return nums;
 }
