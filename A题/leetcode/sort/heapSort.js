b7261b089922b015475e4b9c084320d430e27de1
Ele-Peng <374237925@qq.com>
2020-11-20 11:01:17 +0800

    feat: heapSort

--- A题/leetcode/sort/heapSort.js
+++ A题/leetcode/sort/heapSort.js
@@ -1,49 +1,68 @@
 /**
+ * 交换 nums 中两个节点
+ * @param {*} nums 
+ * @param {*} i 
+ * @param {*} j 
+ */
+function swap(nums, i, j) {
+  let temp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = temp;
+}
+
+/**
+ * 调整堆
+ * @param {*} nums 
+ * @param {*} n nums 的节点数
+ * @param {*} i 对 i 做 heapify 操作
+ */
+function heapify(nums, n, i) {
+  if (i >= n) return ;
+  let leftChild = 2 * i + 1;
+  let rightChild = 2 * i + 2;
+  let max = i;
+  if (leftChild < n && nums[max] < nums[leftChild]) {
+    max = leftChild;
+  }
+  if (rightChild < n && nums[max] < nums[rightChild]) {
+    max = rightChild;
+  }
+  if (max !== i) {
+    swap(nums, max, i);
+    heapify(nums, n, max);
+  }
+}
+
+/**
+ * 构建堆
+ * 从完全二叉树最后一个叶子节点的父节点开始递减构建
+ * @param {*} nums 
+ * @param {*} n 
+ */
+function buildHeap(nums, n) {
+  let lastIndex = n - 1; // 最后一个叶子节点
+  // parent = (i - 1) >> 1:
+  let lastParentIndex = (lastIndex - 1) >> 1; // 父节点 for 最后一个叶子节点
+  for (let i = lastParentIndex; i >= 0; i --) {
+    heapify(nums, n, i);
+  }
+}
+
+/**
  * 堆排序
- * 时间复杂度 O(nlogn)
- * 不稳定
  * @param {*} nums 
+ * @param {*} n 
  */
-function heapSort(nums) {
-  createHeap(nums);
-  console.log(nums);
-  for (let i = nums.length - 1; i > 0; i --) {
-    [nums[i], nums[0]] = [nums[0], nums[i]];
-    adjust(nums, 0, i);
-  }
-  return nums;
-}
-
-/**
- * 构建大顶堆
- * @param {*} nums 
- */
-function createHeap(nums) {
-  const len = nums.length;
-  const start = (len >> 1) - 1;
-  for (let i = start; i >= 0; i --) {
-    adjust(nums, i, len);
+function heapSort(nums, n) {
+  buildHeap(nums, n);
+  for (let i = n - 1; i >= 0; i --) {
+    swap(nums, i, 0);
+    heapify(nums, i, 0);
   }
 }
 
-/**
- * 将第 target 元素进行下沉，孩子节点有比他大的就下沉
- * @param {*} nums 
- * @param {*} target 
- * @param {*} len 
- */
-function adjust(nums, target, len) {
-  for (let i = 2 * target + 1; i < len; i = 2 * i + 1) {
-    // 找到孩子节点中最大的
-    if (i + 1 < len && nums[i + 1] > nums[i]) {
-      i = i + 1;
-    }
-    // 下沉
-    if (nums[i] > nums[target]) {
-      [nums[i], nums[target]] = [nums[target], nums[i]];
-      target = i;
-    } else {
-      break;
-    }
-  }
+function main() {
+  let arr = [2,4,7,8,6,5,1,9];
+  heapSort(arr, arr.length);
+  return arr;
 }
