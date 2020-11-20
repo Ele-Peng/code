9286ee5285e0d371e510a6db898c6a36de85b3c1
Ele-Peng <374237925@qq.com>
2020-11-20 14:28:27 +0800

    feat: bubbleSort

--- A题/leetcode/sort/bubbleSort.js
+++ A题/leetcode/sort/bubbleSort.js
@@ -14,20 +14,44 @@
 
 // 优化：
 // 如果经历 < nums.length 次，就已经有序了（不再进行冒泡）可以提前退出比较
-function bubbleSort() {
-    for (let i = 0; i < nums.length; i ++) {
-        let complete = true;
-        for (let j = 0; j < nums.length - i - 1; j ++) {
-            if (nums[j] > nums[j + 1]) {
-                let temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
-                complete = false;
-            }
-        }
-        if (complete) break;
+// function bubbleSort() {
+//     for (let i = 0; i < nums.length; i ++) {
+//         let complete = true;
+//         for (let j = 0; j < nums.length - i - 1; j ++) {
+//             if (nums[j] > nums[j + 1]) {
+//                 let temp = nums[j];
+//                 nums[j] = nums[j + 1];
+//                 nums[j + 1] = temp;
+//                 complete = false;
+//             }
+//         }
+//         if (complete) break;
+//     }
+//     return nums;
+// }
+
+function main() {
+  let arr = [2,4,7,8,6,5,1,9];
+  bubbleSort(arr, arr.length);
+  return arr;
+}
+function bubbleSort(nums, n) {
+  for (let i = 0; i < n; i ++) {
+    let complete = false;
+    for (let j = 0; j < n - i - 1;j ++) {
+      if (nums[j] > nums[j + 1]) {
+        swap(nums, j, j + 1);
+        complete = true;
+      }
     }
-    return nums;
+    if (!complete) break;
+  }
+}
+
+function swap(nums, i, j) {
+  let temp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = temp;
 }
 
 // bubbleSort([2,4,7,8,6,5,1,9]);
