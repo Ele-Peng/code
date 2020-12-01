b1f6218df44395216cf94578b1a1993e813bb5b1
Ele-Peng <374237925@qq.com>
2020-12-01 16:33:49 +0800

    feat: insertSort

--- A题/leetcode/sort-2/insertSort.js
+++ A题/leetcode/sort-2/insertSort.js
@@ -1,3 +1,9 @@
+function insertSort(nums, n) {
+  for (let i = 1; i < n; i ++) {
+    insert(nums, i);
+  }
+}
+
 function insert(nums, n) {
   let key = nums[n];
   let i = n;
@@ -9,15 +15,8 @@ function insert(nums, n) {
   nums[i] = key;
 }
 
-function insertSort(nums, n) {
-  for (let i = 1; i < n; i ++) {
-    insert(nums, i);
-  }
-}
-
 function main() {
   let arr = [2,4,7,9,8,6,5,1];
   insertSort(arr, arr.length);
   return arr;
 }
-
