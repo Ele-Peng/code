ed06c24a1acf1c9b3c5328280951883d7f484786
ElePeng <374237925@qq.com>
2020-12-01 21:11:53 +0800

    feat: halfSearch - ii

--- A题/leetcode/34.在排序数组中查找元素的第一个和最后一个位置.js
+++ A题/leetcode/34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -11,26 +11,21 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    return midSearch(nums, target);
-};
-var midSearch = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
     while (left <= right) {
         let mid = (left + right) >> 1;
-        if (target < nums[mid]) {
+        if (nums[mid] > target) {
             right = mid - 1;
-        } else if (target > nums[mid]) {
+        } else if (nums[mid] < target) {
             left = mid + 1;
         } else {
             let leftIndex = mid;
-            while(nums[leftIndex - 1] === nums[mid]) {
+            while (nums[leftIndex - 1] === nums[mid])
                 leftIndex --;
-            }
             let rightIndex = mid;
-            while(nums[rightIndex + 1] === nums[mid]) {
+            while (nums[rightIndex + 1] === nums[mid])
                 rightIndex ++;
-            }
             return [leftIndex, rightIndex];
         }
     }
