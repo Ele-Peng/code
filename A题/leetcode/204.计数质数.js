e0ef0a21d5cb1ccee69837a4af92337a5e6b517b
Ele-Peng <374237925@qq.com>
2020-12-03 14:25:20 +0800

    feat: 素数筛

--- A题/leetcode/204.计数质数.js
+++ A题/leetcode/204.计数质数.js
@@ -10,19 +10,17 @@
  * @return {number}
  */
 var countPrimes = function(n) {
-  if (n <= 2) return 0;
-  if (n === 3) return 1;
-  let sum = 1;
-  for (let i = 3; i < n; i ++) {
-    sum = isPrimeNumber(i) ? sum + 1 : sum + 0;
+  let sum = 0;
+  let arr = new Array(n);
+  for (let i = 2; i < n; i ++) {
+    if (!arr[i - 1]) {
+      sum ++;
+      for (let j = i * i; j <= n; j += i) {
+        arr[j - 1] = true;
+      }
+    }
   }
   return sum;
-};
-var isPrimeNumber = function(n) {
-  for (let i = 2; i <= Math.floor(Math.sqrt(n)); i ++) {
-    if (n % i === 0) return false;
-  }
-  return true;
 };
 // @lc code=end
 
