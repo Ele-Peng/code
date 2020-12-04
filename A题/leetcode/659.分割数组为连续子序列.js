5b70a7a415f1c361fcfa06bda50b654c1150e248
ElePeng <374237925@qq.com>
2020-12-05 00:05:16 +0800

    feat: variavle

--- A题/leetcode/659.分割数组为连续子序列.js
+++ A题/leetcode/659.分割数组为连续子序列.js
@@ -10,7 +10,15 @@
  * @return {boolean}
  */
 var isPossible = function(nums) {
-
+    let res = [];
+    let numMap = new Map;
+    let len = nums.length;
+    if (len < 3) return false;
+    for (let i = 0; i < nums.length; i ++) {
+        numMap.has(nums[i]) ? numMap.set(nums[i], numMap.get(nums[i]) + 1) : numMap.set(nums[i], 1);
+    }
+    let max = len;
+    let min = 3;
 };
 // @lc code=end
 
