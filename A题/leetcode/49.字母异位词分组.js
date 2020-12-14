0f4d365b26c7b0c038134729f0420f495e8322f6
ElePeng <374237925@qq.com>
2020-12-14 22:14:45 +0800

    feat: groupAnagrams

--- A题/leetcode/49.字母异位词分组.js
+++ A题/leetcode/49.字母异位词分组.js
@@ -33,17 +33,38 @@
 
 // 优化预处理直接为该数组 + for in Object
 
+// var groupAnagrams = function(strs) {
+//   let strsTemp = {};
+//   for (let i = 0;i < strs.length; i ++) {
+//     let key = strs[i].split("").sort().join("");
+//     strsTemp[key] ? strsTemp[key].push(strs[i]) : strsTemp[key] = [strs[i]];
+//   }
+//   let res = [];
+//   for (let i in strsTemp) {
+//     res.push(strsTemp[i]);
+//   }
+//   return res;
+// };
+
+
+// 第三遍
 var groupAnagrams = function(strs) {
-  let strsTemp = {};
-  for (let i = 0;i < strs.length; i ++) {
+  let map = new Map;
+  for (let i = 0; i < strs.length; i ++) {
     let key = strs[i].split("").sort().join("");
-    strsTemp[key] ? strsTemp[key].push(strs[i]) : strsTemp[key] = [strs[i]];
+    if (map.has(key)) {
+      let arr = map.get(key);
+      arr.push(strs[i]);
+      map.set(key, arr);
+    } else {
+      map.set(key, [ strs[i] ]);
+    }
   }
   let res = [];
-  for (let i in strsTemp) {
-    res.push(strsTemp[i]);
+  for (let key of map) {
+    res.push(key[1]);
   }
   return res;
-};
+} 
 // @lc code=end
 
