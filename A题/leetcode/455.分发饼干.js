6dc0dbbc72e67f04a97014a91160c1bf0c9ebfcc
ElePeng <374237925@qq.com>
2020-12-25 23:07:33 +0800

    feat: findContentChildren

--- A题/leetcode/455.分发饼干.js
+++ A题/leetcode/455.分发饼干.js
@@ -12,19 +12,28 @@
  */
 // 每个孩子最多给一块饼干，不需要出现凑饼干的情况
 var findContentChildren = function(g, s) {
-    let tempG = g.sort((a, b) => a - b);
-    let tempS = s.sort((a, b) => a - b);
-    let res = 0;
-    for (let i = 0, j = 0; i < tempG.length && j < tempS.length; ) {
-        if (tempG[i] <= tempS[j]) {
-            res ++;
-            i ++;
-            j ++;
-        } else {
-            j ++; // 当前饼干小了，找下一块大的饼干
+    const cmp = (a, b) => { return a - b };
+    let tempG = g.sort(cmp);
+    let tempS = s.sort(cmp);
+
+    let count = 0;
+    let pos = -1;
+    for (let i = 0; i < tempS.length; i ++) {
+        if (tempS[i] >= tempG[0]) {
+            pos = i;
+            break;
         }
     }
-    return res;
+    for (let i = 0; i < tempG.length && pos < tempS.length;) {
+        if (tempG[i] <= tempS[pos]) {
+            count += 1;
+            pos += 1;
+            i += 1;
+        } else { 
+            pos += 1;
+        }
+    }
+    return count;
 };
 // @lc code=end
 
