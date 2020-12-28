b441c2751af5650b1a0171646593a9f837088386
ElePeng <374237925@qq.com>
2020-12-28 22:16:53 +0800

    feat: 121 maxProfit O(1)

--- A题/leetcode/121.买卖股票的最佳时机.js
+++ A题/leetcode/121.买卖股票的最佳时机.js
@@ -20,27 +20,39 @@
 // };
 
 
+// var maxProfit = function(prices) {
+//     let n = prices.length;
+//     if (!n) return 0;
+//     let dp = new Array(n);
+//     let k = 1;
+//     for (let i = 0; i < n; i ++) {
+//         dp[i] = new Array(k);
+//         for (let j = 0;j <= k; j ++) {
+//             dp[i][j] = (new Array(2)).fill(0);
+//         }
+//     }
+//     for (let i = 0; i < n; i ++) {
+//         if (i - 1 === -1) { // base case
+//             dp[i][1][0] = 0;
+//             dp[i][1][1] = -prices[i];
+//             continue;
+//         }
+//         dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][1][1] + prices[i]);
+//         dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][0][0] - prices[i]);
+//     }
+//     return dp[n - 1][1][0];
+// };
+
+
+
 var maxProfit = function(prices) {
     let n = prices.length;
-    if (!n) return 0;
-    let dp = new Array(n);
-    let k = 1;
+    let dp_i_0 = 0, dp_i_1 = -Infinity;
     for (let i = 0; i < n; i ++) {
-        dp[i] = new Array(k);
-        for (let j = 0;j <= k; j ++) {
-            dp[i][j] = (new Array(2)).fill(0);
-        }
+        dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+        dp_i_1 = Math.max(dp_i_1, -prices[i]); // 只能进行一次交易
     }
-    for (let i = 0; i < n; i ++) {
-        if (i - 1 === -1) { // base case
-            dp[i][1][0] = 0;
-            dp[i][1][1] = -prices[i];
-            continue;
-        }
-        dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][1][1] + prices[i]);
-        dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][0][0] - prices[i]);
-    }
-    return dp[n - 1][1][0];
+    return dp_i_0;
 };
 // @lc code=end
 
