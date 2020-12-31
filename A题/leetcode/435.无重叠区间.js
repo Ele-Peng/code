d5207ede7e52d5e42690695f3765bcc3077ddfbc
Ele-Peng <374237925@qq.com>
2020-12-31 18:43:42 +0800

    feat: 435 eraseOverIntervals

--- A题/leetcode/435.无重叠区间.js
+++ A题/leetcode/435.无重叠区间.js
@@ -11,30 +11,30 @@ const { lemonchiffon } = require("color-name");
  * @param {number[][]} intervals
  * @return {number}
  */
-var eraseOverlapIntervals = function(intervals) {
-  if (!intervals.length) return 0;
-  return intervals.length - overlapIntervals(intervals);
-};
+// var eraseOverlapIntervals = function(intervals) {
+//   if (!intervals.length) return 0;
+//   return intervals.length - overlapIntervals(intervals);
+// };
 
 
-var overlapIntervals = function(intervals) {
-  let len = intervals.length;
-  const compare = (a, b) => {
-    return a[1] - b[1];
-  }
-  intervals.sort(compare);
+// var overlapIntervals = function(intervals) {
+//   let len = intervals.length;
+//   const compare = (a, b) => {
+//     return a[1] - b[1];
+//   }
+//   intervals.sort(compare);
 
-  let count = 1;
-  let defaultIntervalEnd = intervals[0][1];
+//   let count = 1;
+//   let defaultIntervalEnd = intervals[0][1];
   
-  for (let i = 0; i < len; i ++) {
-    if (intervals[i][0] >= defaultIntervalEnd) {
-      count ++;
-      defaultIntervalEnd = intervals[i][1]; // 更新比较终点
-    }
-  }
+//   for (let i = 0; i < len; i ++) {
+//     if (intervals[i][0] >= defaultIntervalEnd) {
+//       count ++;
+//       defaultIntervalEnd = intervals[i][1]; // 更新比较终点
+//     }
+//   }
 
-  return count;
-};
+//   return count;
+// };
 // @lc code=end
 
