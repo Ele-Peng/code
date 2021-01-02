207ac3d39231268d223fdf72df7524e3304fa515
Ele-Peng <374237925@qq.com>
2021-01-02 16:50:13 +0800

    feat: rotate matrix not use a new matrix

--- A题/leetcode/48.旋转图像.js
+++ A题/leetcode/48.旋转图像.js
@@ -9,24 +9,23 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+// point(x, y) --> point(y, x) --> point(y, n - i + 1)
 var rotate = function(matrix) {
-  let row = matrix.length;
-  let tempMatrix = new Array(row);
-  for (let i = 0; i < row; i ++) {
-    tempMatrix[i] = (new Array(row)).fill(-1);
-  }
-  let rowIndex = 0, colIndex = 0;
-  for (let j = 0; j < row; j ++) {
-    for (let i = row - 1; i >= 0; i --) {
-      tempMatrix[rowIndex][colIndex] = matrix[i][j];
-      colIndex ++;
+  let n = matrix.length;
+  // 对角线 矩阵
+  for (let i = 0; i < n; i ++) {
+    for (let j = 0; j < i; j ++) {
+      let temp = matrix[j][i];
+      matrix[j][i] = matrix[i][j];
+      matrix[i][j] = temp;
     }
-    colIndex = 0;
-    rowIndex ++;
   }
-  for (let i = 0; i < row; i ++) {
-    for (let j = 0; j < row; j ++) {
-      matrix[i][j] = tempMatrix[i][j];
+  // 中心轴对称矩阵
+  for (let i = 0; i < n; i ++) {
+    for (let j = 0; j < (n >> 1); j ++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[i][n - j - 1];
+      matrix[i][n - j - 1] = temp;
     }
   }
   return matrix;
