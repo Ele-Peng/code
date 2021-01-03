c579d9db2f4331c5949c665d8e9691871c8376f6
Ele-Peng <374237925@qq.com>
2021-01-03 11:29:12 +0800

    feat: reverseList

--- A题/leetcode/206.反转链表.js
+++ A题/leetcode/206.反转链表.js
@@ -26,7 +26,7 @@
 
 
 var reverseList = function(head) {
-  if (!head || !head.next) return head;
+  if(!head || !head.next) return head;
   let last = reverseList(head.next);
   head.next.next = head;
   head.next = null;
