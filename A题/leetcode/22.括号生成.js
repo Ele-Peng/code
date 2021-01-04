1036cce1d4e0a5305c4b4b2f6faa798c4acdd743
ElePeng <374237925@qq.com>
2021-01-04 21:53:51 +0800

    feat: generateParenthesis

--- A题/leetcode/22.括号生成.js
+++ A题/leetcode/22.括号生成.js
@@ -9,23 +9,42 @@
  * @param {number} n
  * @return {string[]}
  */
+// var generateParenthesis = function(n) {
+//     let left = 0;
+//     let right = 0;
+//     let str = "";
+//     let res = [];
+//     if (n === 0) return [];
+//     generateParenthesisHelper(left, right, n, str, res);
+//     return res
+// };
+
+// var generateParenthesisHelper = function (left, right, n, str, res) {
+//     if (left === n && right === n) {
+//         res.push(str);
+//         return ;
+//     }
+//     if (left < n) generateParenthesisHelper(left + 1, right, n, str + "(", res);
+//     if (right < left) generateParenthesisHelper(left, right + 1, n, str + ")", res);
+// }
+
+
+
 var generateParenthesis = function(n) {
-    let left = 0;
-    let right = 0;
-    let str = "";
+    let left = 0, right = 0;
     let res = [];
-    if (n === 0) return [];
-    generateParenthesisHelper(left, right, n, str, res);
-    return res
+    if (!n) return res;
+    generateParenthesisHelper(left, right, '', n, res);
+    return res;
 };
 
-var generateParenthesisHelper = function (left, right, n, str, res) {
+var generateParenthesisHelper = function(left, right, path, n, res) {
     if (left === n && right === n) {
-        res.push(str);
+        res.push(path);
         return ;
     }
-    if (left < n) generateParenthesisHelper(left + 1, right, n, str + "(", res);
-    if (right < left) generateParenthesisHelper(left, right + 1, n, str + ")", res);
+    if (left < n) generateParenthesisHelper(left + 1, right, path + '(', n , res);
+    if (right < left) generateParenthesisHelper(left, right + 1, path + ')', n, res);
 }
 // @lc code=end
 
