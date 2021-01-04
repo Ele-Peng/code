24055be6eaf2b74f2612cc9ba27bc553e7c59c3d
ElePeng <374237925@qq.com>
2021-01-04 19:54:45 +0800

    feat: shuffle

--- A题/leetcode/384.打乱数组.js
+++ A题/leetcode/384.打乱数组.js
@@ -26,18 +26,19 @@ Solution.prototype.reset = function() {
  */
 Solution.prototype.shuffle = function() {
   let res = this.nums.slice();
-  for (let i = 0; i < this.nums.length; i ++) {
+  // 洗牌算法
+  const randomFromN2M = (n, m) => {
+    return Math.floor(Math.random() * (m - n + 1)) + n;
+  }
+  let len = res.length;
+  for (let i = 0; i < len; i ++) {
     let temp = res[i];
-    let j = randomFromN2M(i, this.nums.length - 1);
+    let j = randomFromN2M(i, len - 1);
     res[i] = res[j];
     res[j] = temp;
   }
   return res;
 };
-
-function randomFromN2M(n, m) {
-  return Math.ceil(Math.random * (m - n + 1)) + n;
-}
 
 /**
  * Your Solution object will be instantiated and called as such:
