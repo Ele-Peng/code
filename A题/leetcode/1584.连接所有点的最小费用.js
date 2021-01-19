aa9731d94a82a4be1d0b3d5096feef1e544d1852
ElePeng <374237925@qq.com>
2021-01-19 23:51:47 +0800

    feat: union found

---
+++ A题/leetcode/1584.连接所有点的最小费用.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=1584 lang=javascript
+ *
+ * [1584] 连接所有点的最小费用
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minCostConnectPoints = function(points) {
+    let len = points.length;
+    const edges = [];
+    for (let i = 0; i < len; i ++) {
+        for (let j = i + 1; j < len; j ++) {
+            const [xi, yi] = points[i];
+            const [xj, yj] = points[j];
+            edges.push([i, j, Math.abs(xj - xi) + Math.abs(yj - yi)])
+        }
+    }
+    return UF(len, edges);
+};
+
+function UF(len, edges) {
+    const edgesTemp = edges.sort((a, b) => {
+        return a[2] - b[2];
+    });
+    const father = (new Array(len)).map((item, index) => {
+        return index;
+    });
+    let res = 0;
+    // get Father
+    const getFather = (x) => {
+        if (father[x] === x) return x;
+        return father[x] = getFather(fa[x]);
+    }
+    // 这里是点x点y，对应上面的points的索引
+    for (const [x, y, val] of edgesTemp) {
+        const parent_x = getFather(x);
+        const parent_y = getFather(y);
+        if (parent_x !== parent_y) {
+            father[parent_x] = parent_y;
+            res += val;
+        }
+    }
+    return res;
+}
+// @lc code=end
+
