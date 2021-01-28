6e5ac28d7f02fc3c6be63f7a49b8f25e7bc47069
ElePeng <374237925@qq.com>
2021-01-28 23:43:08 +0800

    feat: pivotIndex

---
+++ A题/leetcode/724.寻找数组的中心索引.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=724 lang=javascript
+ *
+ * [724] 寻找数组的中心索引
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let sum = nums.reduce((a, b) => {
+        return a + b;
+    }, 0);
+    let leftSum = 0;
+    for (let i = 0; i < nums.length; i ++) {
+        if (sum - nums[i] - leftSum === leftSum) {
+            return i;
+        } else {
+            leftSum += nums[i];
+        }
+    }
+    return -1;
+};
+// @lc code=end
+
