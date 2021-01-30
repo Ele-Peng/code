98e32d2ab68e6c313a58cc64598dbc866e4005fa
ElePeng <374237925@qq.com>
2021-01-30 23:48:05 +0800

    feat

--- A题/leetcode/724.寻找数组的中心索引.js
+++ A题/leetcode/724.寻找数组的中心索引.js
@@ -13,6 +13,7 @@ var pivotIndex = function(nums) {
     let sum = nums.reduce((a, b) => {
         return a + b;
     }, 0);
+    
     let leftSum = 0;
     for (let i = 0; i < nums.length; i ++) {
         if (sum - nums[i] - leftSum === leftSum) {
@@ -21,7 +22,7 @@ var pivotIndex = function(nums) {
             leftSum += nums[i];
         }
     }
-    
+
     return -1;
 };
 // @lc code=end
