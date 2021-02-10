95a23dbc926879ca54839fdf942d31a05bb30252
ElePeng <374237925@qq.com>
2021-02-10 17:34:32 +0800

    feat: 滑动窗口

--- A题/leetcode/567.字符串的排列.js
+++ A题/leetcode/567.字符串的排列.js
@@ -11,36 +11,36 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-  let need = new Map; // 需要凑齐的字符数
-  let window = new Map; // 窗口中的字符数
+    let need = new Map;
+    let window = new Map;
 
-  let left = 0, right = 0;
-
-  let valid = 0; // 窗口中满足 need 条件的个数
-
-  for (let s of s1) {
-    need.has(s) ? need.set(s, need.get(s) + 1) : need.set(s, 1);
-  }
-
-  while (right < s2.length) {
-    let c = s2[right];
-    right ++;
-    if (need.has(c)) {
-      window.has(c) ? window.set(c, window.get(c) + 1) : window.set(c, 1);
-      if (window.get(c) === need.get(c)) valid ++;
+    for (let i = 0; i < s1.length; i ++) {
+        let c = s1[i];
+        need.has(c) ? need.set(c, need.get(c) + 1) : need.set(c, 1);
     }
 
-    while (right - left >= s1.length) {
-      if (valid === need.size) return true;
-      let d = s2[left];
-      left ++;
-      if (need.has(d)) {
-        if (window.get(d) === need.get(d)) valid --;
-        window.set(d, window.get(d) - 1);
-      }
+    let left = 0, right = 0;
+    let valid = 0;
+
+    while (right < s2.length) {
+        let c = s2[right];
+        right ++;
+        if (need.has(c)) {
+            window.has(c) ? window.set(c, window.get(c) + 1) : window.set(c, 1);
+            if (window.get(c) === need.get(c)) valid ++;
+        }
+
+        while (right - left >= s1.length) {
+            if (valid === need.size) return true;
+            let d = s2[left];
+            left ++;
+            if (need.has(d)) {
+                if (window.get(d) === need.get(d)) valid --;
+                window.set(d, window.get(d) - 1);
+            }
+        }
     }
-  }
-  return false;
+    return false;
 };
 // @lc code=end
 
