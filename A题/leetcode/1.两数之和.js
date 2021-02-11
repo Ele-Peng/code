2740b47c9e2ca2f8a62f41424d32d37c069ed9d4
ElePeng <374237925@qq.com>
2021-02-11 23:45:37 +0800

    feat: 2020 the final leetcode

--- A题/leetcode/1.两数之和.js
+++ A题/leetcode/1.两数之和.js
@@ -36,12 +36,12 @@
 
 // 第三遍 hash
 var twoSum = function(nums, target) {
-  let map = {}
-  for (let i = 0;i < nums.length; i ++) {
+  let map = {};
+  for (let i = 0; i < nums.length; i ++) {
     if (map[target - nums[i]] !== void 0 && map[target - nums[i]] !== i) {
       return [map[target - nums[i]], i];
     }
-    map[nums[i]] = i; // [3, 3]的情况
+    map[nums[i]] = i;
   }
 };
 
