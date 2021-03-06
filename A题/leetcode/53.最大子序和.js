63699488318e6807c6f2b3d398d9113e46d083e0
ElePeng <374237925@qq.com>
2021-03-06 22:54:25 +0800

    feat: maxSubarray

--- A题/leetcode/53.最大子序和.js
+++ A题/leetcode/53.最大子序和.js
@@ -9,81 +9,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var maxSubArray = function(nums) {
-//   let n = nums.length;
-//   if (n === 0) return 0;
-//   let dp = new Array(n);
-//   dp[0] = nums[0];
-//   for (let i = 1; i < n; i ++) {
-//     dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
-//   }
-//   let res = dp[0];
-//   for (let i = 1; i < n; i ++) {
-//     res = Math.max(res, dp[i]);
-//   }
-//   return res;
-// };
-// dp 优化，空间复杂度 O(1)
-// var maxSubArray = function(nums) {
-//   let n = nums.length;
-//   if (n === 0) return 0;
-//   let dp_default = nums[0];
-//   let dp_res = nums[0];
-//   let res = nums[0];
-//   for (let i = 1; i < n; i ++) {
-//     dp_res = Math.max(nums[i], nums[i] + dp_default);
-//     dp_default = dp_res;
-//     res = Math.max(res, dp_res);
-//   }
-//   return res;
-// };
-
-// 前缀和 暴力
-// var maxSubArray = function(nums) {
-//   let prefix = [0];
-//   for (let i = 1; i <= nums.length; i ++) {
-//     prefix[i] = nums[i - 1] + prefix[i - 1];
-//   }
-//   let max = prefix[1];
-//   for (let i = 0; i <= nums.length - 1; i ++) {
-//     for (let j = i + 1; j <= nums.length; j ++) {
-//       let delta = prefix[j] - prefix[i];
-//       if (delta > max) {
-//         max = delta
-//       }
-//     }
-//   }
-//   return max;
-// };
-
-// 动态规划
-// var maxSubArray = function(nums) {
-//   let dp = new Array(nums.length);
-//   let len = nums.length;
-//   if (!len) return 0;
-//   dp[0] = nums[0];
-//   for (let i = 1; i < nums.length; i ++) {
-//     dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-//   }
-//   let res = dp[0];
-//   for (let i = 0;i < nums.length; i ++) {
-//     res = Math.max(dp[i], res);
-//   }
-//   return res;
-// };
 
 var maxSubArray = function(nums) {
-  let len = nums.length;
-  if (!len) return len;
-  let dp_default = nums[0];
-  let dp_res = nums[0];
-  let res = nums[0];
-  for (let i = 1; i < nums.length; i ++) {
-    dp_res = Math.max(nums[i] + dp_default, nums[i]);
-    dp_default = dp_res;
-    res = Math.max(res, dp_res);
+  if (!nums.length) return ;
+  if (nums.length === 1) return nums[0];
+  let prefixSum = [0];
+  for (let i = 1; i <= nums.length; i ++) {
+    prefixSum[i] = nums[i - 1] + prefixSum[i - 1];
   }
-  return res;
+  let max = prefixSum[1];
+  for (let i = 0; i <= nums.length; i ++) {
+    for (let j = i + 1; j <= nums.length; j ++) {
+      max = Math.max(max, prefixSum[j] - prefixSum[i]);
+    }
+  }
+  return max;
 };
 // @lc code=end
 
