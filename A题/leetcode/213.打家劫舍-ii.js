13376e35899cc34a0c0fb5e37b26d79aeee77eba
ElePeng <374237925@qq.com>
2021-04-15 20:49:34 +0800

    feat: 213. rob

--- A题/leetcode/213.打家劫舍-ii.js
+++ A题/leetcode/213.打家劫舍-ii.js
@@ -9,17 +9,33 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-  let len = nums.length;
-  if (len === 1) return nums[0];
-  return Math.max(dp(nums, 0, len - 2), dp(nums, 1, len - 1));
-};
+// var rob = function(nums) {
+//   let len = nums.length;
+//   if (len === 1) return nums[0];
+//   return Math.max(dp(nums, 0, len - 2), dp(nums, 1, len - 1));
+// };
 
-var dp = function (nums, start, end) {
-  let dp_i_1 = 0, dp_i_2 = 0;
-  let dp_i = 0;
+// var dp = function (nums, start, end) {
+//   let dp_i_1 = 0, dp_i_2 = 0;
+//   let dp_i = 0;
+//   for (let i = end; i >= start; i --) {
+//     dp_i = Math.max(dp_i_1, nums[i] + dp_i_2);
+//     dp_i_2 = dp_i_1;
+//     dp_i_1 = dp_i;
+//   }
+//   return dp_i;
+// }
+
+var rob = function(nums) {
+  let n = nums.length;
+  if (n === 1) return nums[0];
+  return Math.max(dp(nums, 0, n - 2), dp(nums, 1, n - 1));
+}
+
+function dp(nums, start, end) {
+  let dp_i = 0, dp_i_1 = 0, dp_i_2 = 0;
   for (let i = end; i >= start; i --) {
-    dp_i = Math.max(dp_i_1, nums[i] + dp_i_2);
+    dp_i = Math.max(nums[i] + dp_i_2, dp_i_1);
     dp_i_2 = dp_i_1;
     dp_i_1 = dp_i;
   }
