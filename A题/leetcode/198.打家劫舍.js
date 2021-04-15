61806a30eebc3ae061b9c7f9da4ed0bcfd567eed
ElePeng <374237925@qq.com>
2021-04-15 20:34:09 +0800

    feat: 198 rob

--- A题/leetcode/198.打家劫舍.js
+++ A题/leetcode/198.打家劫舍.js
@@ -36,16 +36,32 @@
 // };
 
 
+// var rob = function(nums) {
+//   let len = nums.length;
+//   let dp_i_1 = 0, dp_i_2 = 0;
+//   let dp_i = 0;
+//   for (let i = len - 1; i >= 0; i --) {
+//     dp_i = Math.max(dp_i_1, nums[i] + dp_i_2);
+//     dp_i_2 = dp_i_1;
+//     dp_i_1 = dp_i;
+//   }
+//   return dp_i;
+// };
+
+
+
+
 var rob = function(nums) {
-  let len = nums.length;
-  let dp_i_1 = 0, dp_i_2 = 0;
-  let dp_i = 0;
-  for (let i = len - 1; i >= 0; i --) {
-    dp_i = Math.max(dp_i_1, nums[i] + dp_i_2);
-    dp_i_2 = dp_i_1;
-    dp_i_1 = dp_i;
-  }
-  return dp_i;
+  let money = (new Array(nums.length)).fill(-1);
+  return dp(nums, 0, money);
 };
+
+function dp(nums, start, money) {
+  if (start >= nums.length) return 0;
+  if (money[start] !== -1) return money[start];
+  res = Math.max(nums[start] + dp(nums, start + 2, money), dp(nums, start + 1, money));
+  money[start] = res;
+  return res;
+}
 // @lc code=end
 
