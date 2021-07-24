c10446d6622b4ea80934a8975b871b3cae68b6cd
elepeng <374237925@qq.com>
2021-07-24 18:10:44 +0800

    feat: P2102

---
+++ OJ/Luogu/P2102.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr[5][5];
+    int row_max[5];
+    for (int i = 0; i < 5; i ++) {
+        for (int j = 0; j < 5; j ++) {
+            cin >> arr[i][j];
+            // 找出每行最大值
+            if (j == 0) row_max[i] = arr[i][j];
+             else row_max[i] = row_max[i] < arr[i][j] ? arr[i][j] : row_max[i];
+        }
+    }
+    int col_min[5];
+    // 找出每列最小的值
+    for (int i = 0; i < 5; i ++) {
+        for (int j = 0; j < 5; j ++) {
+            if (j == 0) col_min[i] = arr[j][i];
+            else col_min[i] = col_min[i] > arr[j][i] ? arr[j][i] : col_min[i];
+        }
+    }
+    for (int i = 0; i < 5; i ++) {
+        for (int j = 0; j < 5; j ++) {
+            if (row_max[i] == col_min[j]) {
+                cout << i + 1 << " " << j + 1 << " " << row_max[i];
+                return 0;
+            }
+        }
+    }
+    cout << "not found";
+    return 0;
+}
--- OJ/Luogu/P5681.cpp
+++ OJ/Luogu/P5681.cpp
@@ -3,10 +3,11 @@
 #include <algorithm>
 using namespace std;
 
+long long a, b, c;
+
 int main() {
-    unsigned long long a, b, c;
     cin >> a >> b >> c;
-    if (a * a >= b * c) {
+    if (a * a > b * c) {
         cout << "Alice" << endl;
     } else {
         cout << "Bob" << endl;
